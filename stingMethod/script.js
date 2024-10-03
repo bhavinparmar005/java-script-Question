0e7a60b96f7676ad4c68d8af44fd7ab421ef150f
bhavinparmar005 <bhavinparmar005@gmail.com>
2024-10-03 11:24:35 -0700

    he

---
+++ stingMethod/script.js
@@ -0,0 +1,27 @@
+let str ="I Am Parmar Bhavin";
+
+
+// 1.String Length
+//The length property returns the length of a string:
+
+console.log(str.length);
+
+//2.charAt()
+// The charAt() method returns the character at a specified index (position) in a string:
+
+console.log(str.charAt(2));
+
+//3.charCodeAt()
+//The charCodeAt() method returns the code of the character at a specified index in a string:
+
+console.log(str.charCodeAt(3));
+
+//4. String at()
+//Get the third letter of name:
+
+console.log(str.at(3));
+
+//5.
+
+
+
